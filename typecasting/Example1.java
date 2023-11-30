b708082645fea3c661fd64038fef8f79a375c711
nagamanitest <152241046+nagamanitest@users.noreply.github.com>
2023-11-30 12:34:14 +0530

    Delete typecasting directory

--- typecasting/Example1.java
+++
@@ -1,6 +0,0 @@
-package com.simplilearn.typecasting;
-
-public class Example1 {
-
-	// TODO :: WAP for converting int , byte , short  => float , double , long.
-}
--- typecasting/Example2.java
+++
@@ -1,6 +0,0 @@
-package com.simplilearn.typecasting;
-
-public class Example2 {
-
-	// TODO WAP for converting long, float , int => byte , short double.
-}
